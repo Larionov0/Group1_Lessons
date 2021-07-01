3dae6d64f80ca5d31ac8f38595d7c52d9c999e35
Larko_0 <larionov1001@gmail.com>
2021-07-01 18:17:18 +0300

    Django models

--- Sites/FirstDjangoSite/FirstDjangoSite/MainApp/models.py
+++ Sites/FirstDjangoSite/FirstDjangoSite/MainApp/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=1000, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class City(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=1000, blank=True, null=True)
+    country = models.ForeignKey(Country, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f'{self.name} ({self.country})'
+
+
+class Shop(models.Model):
+    name = models.CharField(max_length=60)
+    description = models.TextField(max_length=1000, blank=True, null=True)
+    site_url = models.CharField(max_length=100)
+    cities = models.ManyToManyField(City)
+
+    def __str__(self):
+        return f'{self.name} ({self.site_url})'
+
+
+class Review(models.Model):
+    name = models.CharField(max_length=60)
+    text = models.TextField(max_length=2000)
+    rate = models.IntegerField(default=5)
+    shop = models.ForeignKey(Shop, on_delete=models.PROTECT)
+    archived = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.name} ({self.rate}/10) ({self.shop})'
--- Sites/FirstDjangoSite/FirstDjangoSite/MainApp/views.py
+++ Sites/FirstDjangoSite/FirstDjangoSite/MainApp/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from .models import *
 
 
 def first_view(request):
+    cities = City.objects.all()
+    print(cities)
     return HttpResponse('<p>Ну привет)</p>')
 
